d98cffa7974dfa16024fadb1b8988264b9c9f228
Espe Romero <43826216+EspeRoLo18@users.noreply.github.com>
2021-03-14 21:18:17 -0600

    Primer Commit

---
+++ src/mx/edu/uacm/dominio/Usuario.java
@@ -0,0 +1,35 @@
+package mx.edu.uacm.dominio;
+
+public class Usuario {
+	private int id;
+	private String nombreUsuario;
+	private String mensajeUsuario;
+
+	public Usuario() {
+
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public String getMensajeUsuario() {
+		return mensajeUsuario;
+	}
+
+	public void setMensajeUsuario(String mensajeUsuario) {
+		this.mensajeUsuario = mensajeUsuario;
+	}
+}
